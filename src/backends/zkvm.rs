291d8218807fdf6dc71928c72925558a8bcc646a
jason-nitro <jason@nitro.technology>
2025-08-07 13:51:32 -0500

    Removed redundant getrandom core code, already in SP1

--- src/backends/zkvm.rs
+++ src/backends/zkvm.rs
@@ -11,12 +11,18 @@ use crate::Error;
 use core::mem::MaybeUninit;
 
 // Main function for getrandom 0.3.3 API - note the MaybeUninit
+#[allow(dead_code)]
 pub fn fill_inner(dest: &mut [MaybeUninit<u8>]) -> Result<(), Error> {
+    extern "Rust" { // Call SP1's version
+        fn __getrandom_v03_custom(dest: *mut u8, len: usize) -> Result<(), Error>;
+    }
+
     unsafe {
         __getrandom_v03_custom(dest.as_mut_ptr() as *mut u8, dest.len())
     }
 }
 
+#[allow(dead_code)]
 pub fn inner_u32() -> Result<u32, Error> {
     let mut buf = [MaybeUninit::<u8>::uninit(); 4];
     fill_inner(&mut buf)?;
@@ -25,25 +31,11 @@ pub fn inner_u32() -> Result<u32, Error> {
     Ok(u32::from_ne_bytes(buf))
 }
 
+#[allow(dead_code)]
 pub fn inner_u64() -> Result<u64, Error> {
     let mut buf = [MaybeUninit::<u8>::uninit(); 8];
     fill_inner(&mut buf)?;
     // Safe because fill_inner initialized the bytes
     let buf: [u8; 8] = unsafe { core::mem::transmute(buf) };
     Ok(u64::from_ne_bytes(buf))
-}
-
-// Custom backend function - to be overridden by user implementations
-#[no_mangle]
-pub unsafe extern "Rust" fn __getrandom_v03_custom(
-    dest: *mut u8,
-    len: usize,
-) -> Result<(), Error> {
-    // Should be overridden by the correct sp1 implementation. Previous version was:
-    // unsafe { sp1_zkvm::syscalls::sys_rand(s.as_mut_ptr(), s.len()) };
-
-    for i in 0..len {
-        *dest.add(i) = (i as u8).wrapping_mul(17).wrapping_add(42);
-    }
-    Ok(())
 }
